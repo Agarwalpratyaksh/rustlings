6fd12cede81affc2570985c4e090934389ef71ff
Pratyaksh Agarwal <pratyaksh.agrwl@gmail.com>
2025-08-17 22:09:58 +0530

    init till struct

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,9 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+fn main() {
+    call_me(); // Don't change this line
+}
+
+pub fn call_me(){
+    println!("Hello")
+}
